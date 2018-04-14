4c181eea9a8cc73a9fec00bbd57c3e58608f87dd
Valden <valden219@gmail.com>
2018-04-14 23:08:26 +0300

    Change urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    url(r'', include('polls.urls')),
+    url(r'admin/', admin.site.urls),
 ]
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    url(r'', views.IndexView.as_view(), name='index'),
+    url(r'<int:pk>/', views.DetailView.as_view(), name='detail'),
+    url(r'<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    url(r'<int:question_id>/vote/', views.vote, name='vote'),
 ]
